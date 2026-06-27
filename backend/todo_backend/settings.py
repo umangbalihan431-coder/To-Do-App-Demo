4c82a96da1ad7fe051d266e017581d85ff23a92f
Umang Balihan <anumang1806@gmail.com>
2026-06-27 18:55:39 +0530

    Configure Django for Render

--- backend/todo_backend/settings.py
+++ backend/todo_backend/settings.py
@@ -72,6 +72,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
